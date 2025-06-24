31c6132b004e3f68fe88322d55e1dff41969354e
Junsung Choi <choiexe1@gmail.com>
2025-06-24 16:28:58 +0900

    IntIncrement 이벤트 구체화

--- lib/blocs/int_bloc_event.dart
+++ lib/blocs/int_bloc_event.dart
@@ -1,7 +1,11 @@
 sealed class IntBlocEvent {}
 
 final class IntIncrement extends IntBlocEvent {
-  IntIncrement({this.amount = 1});
+  IntIncrement({this.amount = 1}) {
+    if (amount < 1) {
+      throw Exception('IntIncrement 이벤트의 amount는 1보다 낮을 수 없음');
+    }
+  }
 
   final int amount;
 }
